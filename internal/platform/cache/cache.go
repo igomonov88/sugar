a0fb700805897a599c1ec0553cb1b129dc9ec0cb
Igor Gomonov <i.gomonov@melsoft-games.com>
2020-02-19 18:19:37 +0300

    update cache realization

--- internal/platform/cache/cache.go
+++ internal/platform/cache/cache.go
@@ -10,7 +10,6 @@ import (
 // ErrInvalidConfig is used then some of the config values does not specified
 var ErrInvalidConfig = errors.New("config values does not specified properly")
 
-
 // Config store required properties to use Cache
 type Config struct {
 	// ExpirationTime represents the default value of TimeToLeave parameter of any item in cache. If TTL of the item is
@@ -32,20 +31,22 @@ type Cache struct {
 
 // entry is a value which will store key/value pair in the cache
 type entry struct {
-	key        string
-	value      interface{}
+	key       string
+	value     interface{}
 	expiresAt time.Time
 }
 
 // NewCache knows how to create Cache with given configuration
 func NewCache(cfg Config) (*Cache, error) {
-	if cfg.Size == 0 || cfg.ExpirationTime == 0 {return nil, ErrInvalidConfig}
+	if cfg.Size == 0 || cfg.ExpirationTime == 0 {
+		return nil, ErrInvalidConfig
+	}
 	return &Cache{
-		entryList: list.New(),
-		items: make(map[string]*list.Element, cfg.Size),
-		lock: sync.RWMutex{},
+		entryList:         list.New(),
+		items:             make(map[string]*list.Element, cfg.Size),
+		lock:              sync.RWMutex{},
 		defaultExpiration: cfg.ExpirationTime,
-		initialSize: cfg.Size,
+		initialSize:       cfg.Size,
 	}, nil
 }
 
@@ -56,6 +57,19 @@ func (c *Cache) Add(key string, value interface{}) {
 	c.lock.Unlock()
 }
 
+// add adds element to list if in this process we going out of the initial cache size we will remove appropriate item
+// from the cache
 func add(cache *Cache, key string, value interface{}) {
+	if cache.currentSize >= cache.initialSize {
+		remove(cache)
+	}
 
+}
+
+// remove removes element from the cache
+func remove(cache *Cache) {
+	element := cache.entryList.Back()
+	delete(cache.items, element.Value.(*entry).key)
+	cache.entryList.Remove(element)
+	cache.currentSize--
 }
