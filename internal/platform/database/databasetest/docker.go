901be384119e73d82dc352081eaff55a36458b1b
Igor Gomonov <i.gomonov@melsoft-games.com>
2020-02-24 10:32:47 +0300

    update dpcker.go

--- internal/platform/database/databasetest/docker.go
+++ internal/platform/database/databasetest/docker.go
@@ -17,7 +17,7 @@ type Container struct {
 func StartContainer(t *testing.T) *Container {
 	t.Helper()
 
-	cmd := exec.Command("docker", "run", "-P", "-d", "postgres")
+	cmd := exec.Command("docker", "run", "-P", "-d", "postgres", "-e POSTGRES_PASSWORD=postgres")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	if err := cmd.Run(); err != nil {
