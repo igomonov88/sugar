5dcfb4995a182aaee1efe9a232a6ea404f320adc
Igor Gomonov <i.gomonov@melsoft-games.com>
2020-03-09 00:01:33 +0300

    update docker.go

--- internal/platform/database/databasetest/docker.go
+++ internal/platform/database/databasetest/docker.go
@@ -17,7 +17,7 @@ type Container struct {
 func StartContainer(t *testing.T) *Container {
 	t.Helper()
 
-	cmd := exec.Command("docker", "run", "-P", "-d", "postgres")
+	cmd := exec.Command("docker", "run", "-e", "POSTGRES_HOST_AUTH_METHOD=trust", "-P", "-d", "postgres")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	if err := cmd.Run(); err != nil {
