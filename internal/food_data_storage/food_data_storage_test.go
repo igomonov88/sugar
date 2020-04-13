f5838a804a859c946e0af3c27b35b3243eb039a6
Igor Gomonov <i.gomonov@melsoft-games.com>
2020-04-13 17:13:09 +0300

    update tests in food data storage

--- internal/food_data_storage/food_data_storage_test.go
+++ internal/food_data_storage/food_data_storage_test.go
@@ -13,14 +13,58 @@ func TestFoodDAtaStorage(t *testing.T) {
 	db, teardown := tests.NewUnit(t)
 	defer teardown()
 	ctx := tests.Context()
-	t.Log("Given the need to work with Food records.")
+	t.Log("Given the need to work with food records.")
 	{
 		{
+			// Prepare data for test cases
 			food := food_data_storage.Food{
 				FDCID:       1234,
 				Description: "bounty",
 				BrandOwner:  "mars inc.",
 			}
+			n1 := food_data_storage.Nutrient{
+				Name:     "Protein",
+				Rank:     205,
+				Number:   205,
+				UnitName: "Protein",
+			}
+			n2 := food_data_storage.Nutrient{
+				Name:     "Carbs",
+				Rank:     206,
+				Number:   206,
+				UnitName: "Carbs",
+			}
+			n3 := food_data_storage.Nutrient{
+				Name:     "Fat",
+				Rank:     207,
+				Number:   207,
+				UnitName: "Fat",
+			}
+			fn1 := food_data_storage.FoodNutrient{
+				Type:     "Protein",
+				Amount:   20,
+				Nutrient: n1,
+			}
+			fn2 := food_data_storage.FoodNutrient{
+				Type:     "Carbs",
+				Amount:   30,
+				Nutrient: n2,
+			}
+			fn3 := food_data_storage.FoodNutrient{
+				Type:     "Fat",
+				Amount:   50,
+				Nutrient: n3,
+			}
+			fns := make([]food_data_storage.FoodNutrient, 0, 3)
+			fns = append(fns, fn1)
+			fns = append(fns, fn2)
+			fns = append(fns, fn3)
+			fd := food_data_storage.FoodDetails{
+				Description:   food.Description,
+				FoodNutrients: fns,
+			}
+
+			// Add Food Item to storage and check that everything is OK
 			{
 				err := food_data_storage.AddFood(ctx, db, food, "bounty")
 				if err != nil {
@@ -28,6 +72,7 @@ func TestFoodDAtaStorage(t *testing.T) {
 				}
 				t.Logf("\t%s\tShould be able to add food to storage.", tests.Success)
 			}
+			// Search for Food item in storage and check that everything is OK
 			{
 				foods, err := food_data_storage.Search(ctx, db, food.Description)
 				if err != nil {
@@ -37,55 +82,15 @@ func TestFoodDAtaStorage(t *testing.T) {
 					t.Logf("\t%s\tShould be able search food in storage.", tests.Success)
 				}
 			}
+			// Add Food details to storage and check that everything is OK
 			{
-				n1 := food_data_storage.Nutrient{
-					Name:     "Protein",
-					Rank:     205,
-					Number:   205,
-					UnitName: "Protein",
-				}
-				n2 := food_data_storage.Nutrient{
-					Name:     "Carbs",
-					Rank:     206,
-					Number:   206,
-					UnitName: "Carbs",
-				}
-				n3 := food_data_storage.Nutrient{
-					Name:     "Fat",
-					Rank:     207,
-					Number:   207,
-					UnitName: "Fat",
-				}
-				fn1 := food_data_storage.FoodNutrient{
-					Type:     "Protein",
-					Amount:   20,
-					Nutrient: n1,
-				}
-				fn2 := food_data_storage.FoodNutrient{
-					Type:     "Carbs",
-					Amount:   30,
-					Nutrient: n2,
-				}
-				fn3 := food_data_storage.FoodNutrient{
-					Type:     "Fat",
-					Amount:   50,
-					Nutrient: n3,
-				}
-
-				fns := make([]food_data_storage.FoodNutrient, 0, 3)
-				fns = append(fns, fn1)
-				fns = append(fns, fn2)
-				fns = append(fns, fn3)
-				fd := food_data_storage.FoodDetails{
-					Description:   food.Description,
-					FoodNutrients: fns,
-				}
 				err := food_data_storage.AddDetails(ctx, db, food.FDCID, fd)
 				if err != nil {
 					t.Fatalf("\t%s\tShould be able to add food details to storage: %s", tests.Failed, err)
 				}
 				t.Logf("\t%s\tShould be able to add food details to storage.", tests.Success)
 			}
+			// Get Food details from storage, compare then and check that everything is correct
 			{
 				foodDetails, err := food_data_storage.GetDetails(ctx, db, 1234)
 				if err != nil {
@@ -94,9 +99,24 @@ func TestFoodDAtaStorage(t *testing.T) {
 				if diff := cmp.Diff(food.Description, foodDetails); diff != "" {
 					t.Logf("\t%s\tShould be able to get food details from storage.", tests.Success)
 				}
-				t.Log(foodDetails.FoodNutrients[0].Nutrient.ID)
+				for i := 0; i < len(foodDetails.FoodNutrients)-1; i++ {
+					if foodDetails.FoodNutrients[i].Type == fd.FoodNutrients[i].Type {
+						if !cmp.Equal(foodDetails.FoodNutrients[i].Number, fd.FoodNutrients[i].Number) {
+							t.Fatalf("\t%s\tShould get the same food details from storage: %s", tests.Failed, err)
+						}
+						if !cmp.Equal(foodDetails.FoodNutrients[i].Amount, fd.FoodNutrients[i].Amount) {
+							t.Fatalf("\t%s\tShould get the same food details from storage: %s", tests.Failed, err)
+						}
+						if !cmp.Equal(foodDetails.FoodNutrients[i].Rank, fd.FoodNutrients[i].Rank) {
+							t.Fatalf("\t%s\tShould get the same food details from storage: %s", tests.Failed, err)
+						}
+						if !cmp.Equal(foodDetails.FoodNutrients[i].UnitName, fd.FoodNutrients[i].UnitName) {
+							t.Fatalf("\t%s\tShould get the same food details from storage: %s", tests.Failed, err)
+						}
+					}
+				}
+				t.Logf("\t%s\tShould get the same food details from storage.", tests.Success)
 			}
-
 		}
 	}
 }
